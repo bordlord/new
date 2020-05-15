1aaf1406b678d524fcba5d5bddce4a483e3eac19
bordlord <63213213+bordlord@users.noreply.github.com>
2020-05-15 16:04:29 +0300

    update

--- bot.py
+++ bot.py
@@ -3,8 +3,9 @@ import telebot
 bot = telebot.TeleBot('1127320220:AAFAqMnqePCfmqT7H_kCMpfWhQLCqnNRKW0')
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Сука', 'Блять', 'Пизда', 'Нахуй')
-keyboard1.row('Сука', 'Блять', 'Пизда', 'Нахуй')
+keyboard1.row('Друид', 'Жрец', 'Волшебник', 'Колдун')
+keyboard1.row('Воин', 'Паладин', 'Варвар', 'Плут')
+keyboard1.row('Бард>', 'Следопыт', 'Чародей')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
